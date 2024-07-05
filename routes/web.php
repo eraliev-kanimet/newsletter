e0f7da2232b7395771c7e8cd5669bb3b0c719280
Kanimet <kanimet200127@gmail.com>
2024-07-05 07:27:08 +0600

    Made a route for the frontend application

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('set/locale/{locale}', [SiteController::class, 'locale'])
     ->whereIn('locale', array_keys(config('app.locales')))
     ->name('set.locale');
+
+Route::get('/app', [PageController::class, 'app'])->name('app');
+Route::get('app/{any}', [PageController::class, 'app'])->where('any', '.*');
