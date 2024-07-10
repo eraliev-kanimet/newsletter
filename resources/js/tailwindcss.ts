f9b380701d632013f24209dd37cff41b02734182
Kanimet <kanimet200127@gmail.com>
2024-07-10 16:31:44 +0600

    Made header: button to switch language to alternative, button to switch theme

--- resources/js/tailwindcss.ts
+++ resources/js/tailwindcss.ts
@@ -1,21 +1,28 @@
 
-export function tailwindcssApplyTheme() {
-    const theme = localStorage.getItem('theme');
+export function tailwindcssApplyTheme(theme: string|null): string {
     const root = document.documentElement;
 
     if (theme === 'dark') {
         root.classList.add('dark');
         root.classList.remove('light');
+
+        return 'dark'
     } else if (theme === 'light') {
         root.classList.add('light');
         root.classList.remove('dark');
+
+        return 'light'
     } else {
         if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
             root.classList.add('dark');
             root.classList.remove('light');
+
+            return 'dark'
         } else {
             root.classList.add('light');
             root.classList.remove('dark');
+
+            return 'light'
         }
     }
 }
