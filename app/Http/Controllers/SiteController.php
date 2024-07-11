ebf4817b4c615147ffac245484b35e3be9af4348
Kanimet <kanimet200127@gmail.com>
2024-07-11 07:14:22 +0600

    Refactored the code

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -8,6 +8,8 @@ class SiteController extends Controller
 {
     public function locale(string $locale)
     {
+        $this->notValidParameter($locale, array_keys(config('app.locales')));
+
         App::setLocale($locale);
 
         session(['locale' => $locale]);
--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('set/locale/{locale}', [SiteController::class, 'locale'])
-    ->whereIn('locale', array_keys(config('app.locales')))
-    ->name('set.locale');
+Route::get('set/locale/{locale}', [SiteController::class, 'locale'])->name('set.locale');
 
 Route::get('/app', [PageController::class, 'app'])->name('app');
 Route::get('app/{any}', [PageController::class, 'app'])->where('any', '.*');
