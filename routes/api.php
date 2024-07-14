c6fb4169075d857d367e4726d9d912b4b0b775f6
Kanimet <kanimet200127@gmail.com>
2024-07-14 23:28:34 +0600

    I've created a route for the api - "Messages."

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\MessageController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
+
+Route::apiResource('messages', MessageController::class);
