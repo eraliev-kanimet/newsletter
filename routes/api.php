80df843eb94936f1212ae0c138b8a615dc600b64
Kanimet <kanimet200127@gmail.com>
2024-07-15 23:09:44 +0600

    Created routes for the api 'receivers'

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Controllers\Api\MessageController;
+use App\Http\Controllers\Api\ReceiverController;
 use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', UserController::class);
 
 Route::apiResource('messages', MessageController::class);
+
+Route::apiResource('receivers', ReceiverController::class);
